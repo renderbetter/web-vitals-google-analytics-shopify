4ccd5232b0fbfd6e2dcedf45257b3f8f3c632022
James Koshigoe <james@jameskoshigoe.com>
2023-02-12 02:20:50 -0800

    Cleaning up for public usage

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ Simply add this script to your theme and load it roughly above the closing `</bo
 <script src="{{ 'web-vitals-ga.js' | asset_url }}" data-perf-template="{{ template }}" defer></script>
 ```
 
+## Data Attributes
+
+The following data attributes can be used with the script:
+
+| Attribute  | Description |
+| ------------- | ------------- |
+| `data-perf-template`  | Sets the Shopify template used (this will show up in the event label) |
+| `data-perf-action-prefix`  | Sets the prefix in front of the event. Defaults to `Web Vitals: ` |
+| `data-perf-category`  | Overrides the event category. Defaults to `Web Vitals by Render Better` |
+
 ## Made with ❤️ by the [Render Better](https://www.renderbetter.com) team
 
 Render Better is on a mission to make the web faster and better! If you care about the open web and think site speed/web performance matters a lot, you are one of us. [Learn more about us here](https://www.renderbetter.com/about)!
--- src/web-vitals-ga.ts
+++ src/web-vitals-ga.ts
@@ -1,3 +1,13 @@
+/**!
+ * Core Web Vitals in Google Analytics for Shopify
+ * https://github.com/RenderBetter/web-vitals-google-analytics-shopify
+ *
+ * Copyright (c) Render Better.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import * as webVitals from 'web-vitals';
 import type {Metric} from 'web-vitals';
 
@@ -46,9 +56,11 @@ const sendMetricToGa = function (metric: {name: Metric['name']; value: Metric['v
 
     const debug = attributes.debug === 'true';
     const shopifyTemplate: string | undefined = attributes.perfTemplate;
+    const actionPrefix = attributes.perfActionPrefix || 'Web Vitals: ';
 
-    const action = `Web Vitals: ${metric.name}`;
-    const label = shopifyTemplate;
+    const eventCategory = attributes.perfCategory || 'Web Vitals by Render Better';
+    const eventAction = `${actionPrefix}${metric.name}`;
+    const eventLabel = shopifyTemplate || null;
 
     if (debug) {
         console.log('[RB Analytics] Collecting', metric);
@@ -58,9 +70,9 @@ const sendMetricToGa = function (metric: {name: Metric['name']; value: Metric['v
         // UA data collection model: https://support.google.com/analytics/answer/11091422#universal-analytics
         // event fields: https://developers.google.com/analytics/devguides/collection/analyticsjs/events#event_fields
         const evt = {
-            eventCategory: 'Render Better Analytics',
-            eventAction: action, // LCP, CLS, FID, etc.
-            eventLabel: label,
+            eventCategory,
+            eventAction, // LCP, CLS, FID, etc.
+            eventLabel,
             eventValue: metric.value < 0 ? Math.round(metric.value * 1000) : metric.value, // 3.5
             nonInteraction: true,
             transport: 'beacon',
@@ -69,14 +81,14 @@ const sendMetricToGa = function (metric: {name: Metric['name']; value: Metric['v
     } else if (isGA4Enabled()) {
         // GA 4 events: https://developers.google.com/analytics/devguides/migration/ua/analyticsjs-to-gtagjs
         const evt = {
-            event_category: 'Render Better Analytics',
-            event_action: action,
-            event_label: label,
+            event_category: eventCategory,
+            event_action: eventAction,
+            event_label: eventLabel,
             value: metric.value,
             non_interaction: true,
             transport: 'beacon',
         };
-        sendToGa('event', action, evt);
+        sendToGa('event', eventAction, evt);
     }
 };
 
@@ -85,3 +97,5 @@ webVitals.onCLS(sendMetricToGa);
 webVitals.onFID(sendMetricToGa);
 webVitals.onINP(sendMetricToGa);
 webVitals.onTTFB(sendMetricToGa);
+
+export {};
